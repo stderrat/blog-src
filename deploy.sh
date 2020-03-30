0b4deaa5b9ca4cbfc81ff9264a41c2ff104074dd
Thomas Jungbauer <tjungbau@redhat.com>
2020-03-30 07:37:10 +0200

    added template and deploy-scipt

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+current_dir=$PWD
+current_date=`date +%Y%M%d:%H%m`
+
+echo "Pushing local changes"
+
+git status
+git add .
+git commit -m "changes of $current_date"
+git push -u origin master
+
+cd public/
+
+echo "Publish Blog"
+cp -r * ../../stderrat.github.io
+cd ../../stderrat.github.io
+git status
+git add .
+git commit -m "changes of $current_date"
+git push -u origin master
