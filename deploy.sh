5d86ec3253bf9afc0cafed0659770add2c473867
Thomas Jungbauer <tjungbau@redhat.com>
2020-04-12 16:35:52 +0200

    changes of 20203512:1604

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,22 @@
 current_dir=$PWD
 current_date=`date +%Y%M%d:%H%m`
 
+
 echo "pulling latest changes"
 git pull
 cd ../stderrat.github.io
 git pull
 cd $current_dir
+
+if ! [ -x "$(command -v node)" ]; then
+  echo 'Error: nodejs not found, Consider installing it.' >&2
+  echo "Skipping Search Index ... new posts will not be found"
+else
+  echo "Creating Search Index"
+  node ./build-index.js
+fi
+
+sleep 5
 
 echo "Pushing local changes"
 
--- themes/stderr-ocp/js/scripts.js
+++ themes/stderr-ocp/js/scripts.js
@@ -1,6 +1,14 @@
+/* window.onload function */
+function start()
+{
+  externalLinks();
+  startlunr();
+  scroll2top();
+}
+
 /** External Links **/
 
-function externalLinks() { if (!document.getElementsByTagName) return; var anchors = document.getElementsByTagName("a"); for (var i=0; i<anchors.length; i++) { var anchor = anchors[i]; if (anchor.getAttribute("href") && anchor.getAttribute("rel") == "external") anchor.target = "_blank"; } } window.onload = externalLinks;
+function externalLinks() { if (!document.getElementsByTagName) return; var anchors = document.getElementsByTagName("a"); for (var i=0; i<anchors.length; i++) { var anchor = anchors[i]; if (anchor.getAttribute("href") && anchor.getAttribute("rel") == "external") anchor.target = "_blank"; } };
 
 /** jQuery Easing **/
 
@@ -24,3 +32,18 @@ $(document).ready(function() {
 	});
 });
 
+
+function scroll2top() {
+  mybutton = document.getElementById("myBtn");
+  
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+}
